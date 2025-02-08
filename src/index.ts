bd8aa6afec0041119dd1121e8a82aa4e630b4391
Abhisek Dash <abhisekdash6931@gmail.com>
2025-02-08 17:32:08 +0530

    Implemented shopping cart logic with price API integration and temporary db.json changes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import ShoppingCart from "./cart";
 import { execSync } from "child_process";
+import os from "os";
+
+// Detect OS and use appropriate commands
+const isWindows = os.platform() === "win32";
+
+const copyCmd = isWindows ? "copy db.json db.temp.json" : "cp db.json db.temp.json";
+const moveCmd = isWindows ? "move /Y db.temp.json db.json" : "mv db.temp.json db.json";
 
 // Backup db.json before running
-execSync("cp db.json db.temp.json");
+execSync(copyCmd, { stdio: "inherit" });
 
 process.on("exit", () => {
-    execSync("mv db.temp.json db.json");
+    execSync(moveCmd, { stdio: "inherit" });
 });
 
 (async () => {
